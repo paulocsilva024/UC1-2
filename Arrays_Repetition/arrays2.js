17572f080226476e5886157da0c63e3fe318e90f
Paulo César <paulocsilva024@gmail.com>
2026-07-08 10:15:42 -0300

    Update arrays2.js

--- Arrays_Repetition/arrays2.js
+++ Arrays_Repetition/arrays2.js
@@ -1,20 +1,32 @@
 const rl = require("readline-sync");
 
-let notas = [];
-let soma = 0;
+// let notas = [];
+// let soma = 0;
 
-for (let i = 0; i < 4; i++) {
+// for (let i = 0; i < 4; i++) {
+//     let nota = rl.questionFloat(`Nota ${i + 1}: `);
+//     notas.push(nota);
+//     soma += nota;
+// }
 
-    let nota = rl.questionFloat(`Nota ${i + 1}: `);
+// let media = soma / notas.length;
+// console.log();
+// console.log("Média:", media.toFixed(2));
 
-    notas.push(nota);
-
-    soma += nota;
-
-}
-
-let media = soma / notas.length;
-
-console.log();
-
-console.log("Média:", media.toFixed(2));
+let array = []
+//Adicionar no final da lista .push(elemento)
+array.push('a','b','c','d')
+console.log(`Está assim: ${array}`)
+//Remover no final da lsta .pop()
+array.pop()
+console.log(`Está assim: ${array}`)
+//Adicionar no início da lista .unshift(elemento)
+array.unshift('Novo 0')
+console.log(`Está assim: ${array}`)
+//Remover no início da lista .shift()
+array.shift()
+console.log(`Está assim: ${array}`)
+//Alterar, remover, independentemente de onde está na lista
+// .splice(start: num do elemento, deleteCount: num, Add elemente)
+array.splice(2, 0, 'Among') //
+console.log(`Está assim: ${array}`)
